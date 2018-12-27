53357d31fe98b86f5e23a60b78d839bb73674107
gazandic <gazandic@gmail.com>
2018-12-27 17:09:49 +0700

    bot logic

---
+++ bot.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        "time"
+        "strconv"
+        base64 "encoding/base64"
+
+        "golang.org/x/net/context"
+        "golang.org/x/oauth2"
+        "golang.org/x/oauth2/google"
+        "google.golang.org/api/gmail/v1"
+
+	"github.com/subosito/gotenv"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        tokFile := "token.json"
+        tok, err := tokenFromFile(tokFile)
+        if err != nil {
+                tok = getTokenFromWeb(config)
+                saveToken(tokFile, tok)
+        }
+        return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+        fmt.Printf("Go to the following link in your browser then type the "+
+                "authorization code: \n%v\n", authURL)
+
+        var authCode string
+        if _, err := fmt.Scan(&authCode); err != nil {
+                log.Fatalf("Unable to read authorization code: %v", err)
+        }
+
+        tok, err := config.Exchange(context.TODO(), authCode)
+        if err != nil {
+                log.Fatalf("Unable to retrieve token from web: %v", err)
+        }
+        return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+        f, err := os.Open(file)
+        if err != nil {
+                return nil, err
+        }
+        defer f.Close()
+        tok := &oauth2.Token{}
+        err = json.NewDecoder(f).Decode(tok)
+        return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+        fmt.Printf("Saving credential file to: %s\n", path)
+        f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+        if err != nil {
+                log.Fatalf("Unable to cache oauth token: %v", err)
+        }
+        defer f.Close()
+        json.NewEncoder(f).Encode(token)
+}
+
+func getNewMessages(srv *gmail.Service, bot *tb.Bot, previous time.Time) {
+        user := "me"
+        rThread, err := srv.Users.Threads.Get(user, os.Getenv("THREAD_ID")).Do()
+        
+        if err != nil || rThread == nil {
+                log.Fatalf("Unable to retrieve thread: %v", err)
+        }
+
+        id, err := strconv.Atoi(os.Getenv("YOUR_ID"))
+        if err != nil {
+                log.Fatalf("Unable to retrieve id on env: %v", err)
+        }
+        fmt.Println("Threads:")
+        for _, l := range rThread.Messages {
+                if l.InternalDate < (previous.Unix() * 1000) {
+                        continue
+                }
+                b, _ := base64.StdEncoding.DecodeString(l.Payload.Parts[0].Body.Data)
+                b2, _ := base64.StdEncoding.DecodeString(l.Payload.Parts[1].Body.Data)
+                bot.Send((&tb.User{ ID: id}), string(b) + string(b2), tb.ModeMarkdown)
+        }
+}
+
+func main() {
+	gotenv.Load()
+        previous := time.Now()
+	bot, err := tb.NewBot(tb.Settings{
+		Token:  os.Getenv("TELEGRAM_TOKEN"),
+		Poller: &tb.LongPoller{Timeout: 20 * time.Second},
+		Reporter: func(err error) {
+			fmt.Println(err)
+		},
+	})
+
+        b, err := ioutil.ReadFile("credentials.json")
+        if err != nil {
+                log.Fatalf("Unable to read client secret file: %v", err)
+        }
+
+        // If modifying these scopes, delete your previously saved token.json.
+        config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+        if err != nil {
+                log.Fatalf("Unable to parse client secret file to config: %v", err)
+        }
+        client := getClient(config)
+
+        srv, err := gmail.New(client)
+        if err != nil {
+                log.Fatalf("Unable to retrieve Gmail client: %v", err)
+        }
+
+        for {
+                getNewMessages(srv, bot, previous)
+                previous = time.Now()
+                time.Sleep(120 * time.Second)
+        }
+
+}
